eaa275fcce73d9f1d57c620c861385cd3e62c472
maefju <matthias.nasel@googlemail.com>
2026-08-04 17:02:31 +0200

    change to de no deadkeys

--- .config/hypr/input.lua
+++ .config/hypr/input.lua
@@ -4,8 +4,8 @@
 
 hl.config({
     input = {
-        kb_layout    = "us",
-        kb_variant   = "",
+        kb_layout    = "de",
+        kb_variant   = "nodeadkeys",
         kb_model     = "",
         kb_options   = "grp:alt_shift_toggle",
         kb_rules     = "",
